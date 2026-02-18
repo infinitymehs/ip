712ddacefa3ef51c392d2f5d4cc3153bf8cbcdb7
infinitymehs <106293915+infinitymehs@users.noreply.github.com>
2026-02-18 16:37:46 +0800

    added saving feature
    1. added save function

--- src/main/java/meowth/task/TaskList.java
+++ src/main/java/meowth/task/TaskList.java
@@ -1,4 +1,10 @@
 package meowth.task;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class TaskList {
     private Task[] tasks;
     private int numTask;
@@ -14,6 +20,12 @@ public class TaskList {
             numTask++;
             System.out.println("Added: " + newTask.getTaskName());
             System.out.println(String.format("Now you have %d tasks in the list!", numTask));
+            try{
+                saveTaskList();
+            } catch (IOException e){
+                System.out.println("Sorry something very wrong happened");
+            }
+            
         } else{
             System.out.println("Task list is full! Even Team Rocket isn't as busy :(");
         }
@@ -37,6 +49,11 @@ public class TaskList {
             tasks[idx].setDone(true);
             System.out.println("This task is marked as done! Out of the way!");
             System.out.println(String.format("[X] %s",tasks[idx].getTaskName()));
+            try{
+                saveTaskList();
+            } catch (IOException e){
+                System.out.println("Please nothing enter here");
+            }
         }
         else{
             System.out.println("Task does not exist!");
@@ -48,9 +65,60 @@ public class TaskList {
             tasks[idx].setDone(false);
             System.out.println("This task is marked as undone :(");
             System.out.println(String.format("[ ] %s",tasks[idx].getTaskName()));
+            try{
+                saveTaskList();
+            } catch (IOException e){
+                System.out.println("Please nothing enter here");
+            }
         }
         else{
             System.out.println("Task does not exist!");
         }
+    }
+
+    public void saveTaskList() throws IOException{
+        // not the most efficient but temporarily finish this week's task
+        // because its cny TvT
+
+        //tldr rewrite the entire list each time the function is calleds
+        String filepath = "saved_tasklist.txt";
+        File f = new File(filepath);
+        String textToAdd = "";
+        for (Task task: tasks){
+            String line = "";
+            if (task instanceof ToDo) {
+                // line: "ToDo taskName true"
+                line += "ToDo ";
+                line += task.getTaskName();
+                line += " ";
+                line += String.valueOf(task.getDone());
+                line += "\n";
+            } else if (task instanceof Deadline){
+                // line: "Deadline taskName true deadline"
+                line += "Deadline ";
+                line += task.getTaskName();
+                line += " ";
+                line += String.valueOf(task.getDone());
+                line += " ";
+                line += Deadline.getDeadline();
+                line += "\n";
+            } else if (task instanceof Event){
+                // line: "Event taskName true from to"
+                line += "Event ";
+                line += task.getTaskName();
+                line += " ";
+                line += String.valueOf(task.getDone());
+                line += " ";
+                line += Event.getFrom();
+                line += " ";
+                line += Event.getTo();
+                line += "\n";
+            }
+            textToAdd += line;
+        }
+        FileWriter fw = new FileWriter(filepath);
+        fw.write(textToAdd);
+        fw.close();
+        
     }
 }
